7a5c9203deda205669ab672de86d7a7b85e7286a
Cooper Larson <cooper.larson1@gmail.com>
2024-08-25 12:03:29 -0600

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conan import ConanFile
+from conan.tools.meson import Meson
+from conan.tools.files import copy
+
+
+class Pkg(ConanFile):
+    name = "forti_api"
+    version = "0.1.0"
+    author = "Cooper Larson | cooper.larson1@gmail.com"
+    url = ""
+    description = "FortiGate API interface"
+    topics = ("c++", "security", "DNS blocklists", "fail2ban")
+    settings = "os", "compiler", "arch", "build_type"
+    requires = [
+        'nlohmann_json/3.11.3',
+        'json-schema-validator/2.3.0',
+        'libcurl/8.9.1'
+    ]
+    generators = "PkgConfigDeps", "MesonToolchain"
+    exports_sources = "meson.build", "include/*"
+    implements = ["auto_header_only"]
+
+    def layout(self):
+        self.folders.source = '.'
+        self.folders.build = 'build/meson'
+        self.folders.generators = 'build/generators'
+        self.folders.package = 'build/package'
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure()
+        meson.build()
+
+    def test(self):
+        meson = Meson(self)
+        meson.test()
+
+    def package(self):
+        copy(self, "*.hpp", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        self.cpp_info.includedirs = ['include']
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+    def package_id(self):
+        self.info.clear()
---
+++ include/FortiAPI.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Cooper Larson on 8/25/24.
+//
+
+#ifndef FORTI_API_H
+#define FORTI_API_H
+
+#include <string.h>
+
+class Auth {
+    std::string ip, url, cert_path;
+
+public:
+};
+
+#endif //FORTI_API_H
