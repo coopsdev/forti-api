4fa0aa15eb43bdbc58b6742aa0f692cec0251601
Cooper Larson <cooper.larson1@gmail.com>
2024-09-02 10:29:06 -0600

    simplify policy serialization

--- include/forti_api/firewall.hpp
+++ include/forti_api/firewall.hpp
@@ -6,7 +6,42 @@
 #define FORTI_API_FIREWALL_HPP
 
 #include "api.hpp"
-#include "types/policy.hpp"
+
+struct Module { std::string name, q_origin_key; };
+
+struct Interface : public Module {
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Interface, name, q_origin_key)
+};
+
+struct Address : public Module {
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Address, name, q_origin_key)
+};
+
+struct Service : public Module {
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(Service, name, q_origin_key)
+};
+
+struct FirewallPolicy {
+    unsigned int policyid, q_origin_key, uuid_idx;
+    std::vector<Interface> srcintf, dstintf;
+    std::vector<Address> srcaddr, dstaddr;
+    std::vector<Service> service;
+    std::string status, name, action, ssl_ssh_profile, av_profile, webfilter_profile, dnsfilter_profile,
+                nat, inbound, outbound, natinbound, natoutbound, comments, vlan_filter;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(FirewallPolicy, policyid, q_origin_key, uuid_idx,
+                                                srcintf, dstintf, srcaddr, dstaddr, service,
+                                                status, name, action, ssl_ssh_profile,
+                                                av_profile, webfilter_profile, dnsfilter_profile, nat,
+                                                inbound, outbound, natinbound, natoutbound, comments, vlan_filter)
+};
+
+struct FirewallPoliciesResponse : public Response {
+    std::vector<FirewallPolicy> results;
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(FirewallPoliciesResponse, http_method, size, matched_count, next_idx,
+                                                revision, vdom, path, name, status, http_status, serial, version, build, results)
+};
 
 namespace FortiGate {
 
